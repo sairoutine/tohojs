49608e2830cd4a46f3ff6b365bacb9d6076de349
sairoutine <sairoutine@gmail.com>
2016-02-29 23:25:32 +0900

    add character animation

--- src/js/object/character.js
+++ src/js/object/character.js
@@ -15,6 +15,7 @@ var Character = function(scene) {
 	// Game インスタンス
 	this.game = scene.game;
 
+	this.frame_count = 0;
 	// x座標
 	this.x = 0;
 	// y座標
@@ -29,15 +30,19 @@ var Character = function(scene) {
 //_.extend(Character.prototype, BaseObject.prototype);
 //_.extend(Character, BaseObject);
 
+// 自機のスプライトサイズ
 Character.prototype.WIDTH  = 32;
 Character.prototype.HEIGHT = 48;
 
+// Nフレーム毎に自機をアニメーション
+Character.prototype.ANIMATION_SPAN = 2;
 
 
 // 初期化
 Character.prototype.init = function() {
 	//BaseObject.prototype.init.apply(this, arguments);
 
+	// 自機の初期位置
 	this.x = (this.scene.width / 2);
 	this.y = ( this.scene.height - 100);
 };
@@ -45,6 +50,22 @@ Character.prototype.init = function() {
 // フレーム処理
 Character.prototype.run = function(){
 	//BaseObject.prototype.run.apply(this, arguments);
+	this.frame_count++;
+
+	// Nフレーム毎に自機をアニメーション
+	if(this.frame_count % this.ANIMATION_SPAN === 0) {
+		this.indexX++;
+		// 自機が未移動状態かつスプライトを全て表示しきったら
+		if(this.indexY === 0 && this.indexX > 7) {
+			// 最初のスプライトに戻る
+			this.indexX = 0 ;
+		}
+		// 自機が移動状態かつスプライトを全て表示しきったら
+		else if((this.indexY === 1 || this.indexY === 2) && this.indexX > 7) {
+			// 移動中を除く最初のスプライトに戻る
+			this.indexX = 4 ;
+		}
+	}
 };
 
 // 画面更新
