798abf0dffa4e82ddbd9780e250126bfda02facf
sairoutine <sairoutine@gmail.com>
2016-03-07 01:04:03 +0900

    split bullets_type and bullets_params

---
+++ src/js/data/bullets_type.js
@@ -0,0 +1,46 @@
+
+var __bulletTypes = [
+  {
+    'image':            0,
+    'indexX':           3,
+    'indexY':           4,
+    'width':           16,
+    'height':          32,
+    'collisionWidth':  16,
+    'collisionHeight': 32,
+    'rotate':          true
+  },
+  {
+    'image':            0,
+    'indexX':           3,
+    'indexY':           3,
+    'width':           16,
+    'height':          32,
+    'collisionWidth':  16,
+    'collisionHeight': 32,
+    'rotate':          true
+  },
+  {
+    'image':            0,
+    'indexX':           1,
+    'indexY':          15,
+    'width':           16,
+    'height':          16,
+    'collisionWidth':  16,
+    'collisionHeight': 16,
+    'rotate':          true
+  },
+  {
+    'image':            0,
+    'indexX':           5,
+    'indexY':           0,
+    'width':           16,
+//    'height':          32,
+    'height':          256,
+    'collisionWidth':  16,
+    'collisionHeight': 32,
+    'rotate':          true
+  },
+] ;
+
+module.exports = __bulletTypes;
