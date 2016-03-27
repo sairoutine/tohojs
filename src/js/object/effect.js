e6730fff26eabe49a53c7c826ee6d9b449d6d707
sairoutine <sairoutine@gmail.com>
2016-03-27 20:01:02 +0900

    add enemy vanished effect

--- src/js/object/effect.js
+++ src/js/object/effect.js
@@ -12,6 +12,9 @@ var BaseObject = require('./base');
 var Effect = function(id, scene) {
 	// 継承元new呼び出し
 	BaseObject.apply(this, arguments);
+
+	//TODO:
+	this.preCanvas = [];
 };
 
 // 基底クラスを継承
@@ -20,7 +23,7 @@ _.extend(Effect, BaseObject);
 
 // 初期化
 Effect.prototype.init = function(obj) {
-	// アイテムの初期位置は敵の位置
+	// エフェクトの位置は敵のいた位置
 	this.x = obj.x;
 	this.y = obj.y;
 };
@@ -28,10 +31,44 @@ Effect.prototype.init = function(obj) {
 // フレーム処理
 Effect.prototype.run = function(){
 	BaseObject.prototype.run.apply(this, arguments);
+
+	if(this.end_count < this.frame_count) {
+		this.stage.effectmanager.remove(this.id);
+	}
 };
 
 // 描画
 Effect.prototype.updateDisplay = function() {
+	// TODO:
+	this.width = 32;
+	this.end_count = 10;
+
+	var x = Math.round(this.x);
+	var y = Math.round(this.y);
+	var r = Math.round(this.width * this.frame_count * 0.1);
+
+	if(this.preCanvas[this.frame_count] === undefined) {
+		var cvs = document.createElement('canvas');
+		cvs.width = r*2 + 4;
+		cvs.height = r*2 + 4;
+		var ctx = cvs.getContext('2d');
+
+		ctx.beginPath();
+		ctx.fillStyle = 'rgb(255, 255, 255)';
+		ctx.globalAlpha = (this.end_count - this.frame_count + 1) * 0.05;
+		ctx.arc(r+2, r+2, r, 0, Math.PI * 2);
+		ctx.fill();
+
+		ctx.beginPath();
+		ctx.strokeStyle = 'rgb(255, 255, 255)';
+		ctx.globalAlpha = (this.end_count - this.frame_count + 1) * 0.1;
+		ctx.lineWidth = 3;
+		ctx.arc(r+2, r+2, r, 0, Math.PI * 2);
+		ctx.stroke();
+
+		this.preCanvas[this.frame_count] = cvs;
+	}
+	this.game.surface.drawImage(this.preCanvas[this.frame_count], x-r-2, y-r-2);
 
 };
 
