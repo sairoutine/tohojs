9fd368b312b9c2975c586a3c3b27b5d027409748
sairoutine <sairoutine@gmail.com>
2016-03-27 20:18:07 +0900

    compile js

--- public/js/main.js
+++ public/js/main.js
@@ -18020,9 +18020,17 @@ var Effect = function(id, scene) {
 _.extend(Effect.prototype, BaseObject.prototype);
 _.extend(Effect, BaseObject);
 
+// 何フレームで消滅するか
+Effect.prototype.VANISH_FRAME = 10;
+
+// エフェクトのサイズ
+Effect.prototype.spriteWidth  = function() { return 32; };
+Effect.prototype.spriteHeight = function() { return 32; };
+
+
 // 初期化
 Effect.prototype.init = function(obj) {
-	// アイテムの初期位置は敵の位置
+	// エフェクトの位置は敵のいた位置
 	this.x = obj.x;
 	this.y = obj.y;
 };
@@ -18030,11 +18038,40 @@ Effect.prototype.init = function(obj) {
 // フレーム処理
 Effect.prototype.run = function(){
 	BaseObject.prototype.run.apply(this, arguments);
+
+	// 時間切れ消滅判定
+	if(this.VANISH_FRAME <= this.frame_count) {
+		this.stage.effectmanager.remove(this.id);
+	}
 };
 
 // 描画
 Effect.prototype.updateDisplay = function() {
+	var x = Math.round(this.x);
+	var y = Math.round(this.y);
+	var r = Math.round(this.spriteWidth() * this.frame_count * 0.1);
 
+	var cvs = document.createElement('canvas');
+	cvs.width = r*2 + 4;
+	cvs.height = r*2 + 4;
+	var ctx = cvs.getContext('2d');
+
+	// 円の中
+	ctx.beginPath();
+	ctx.fillStyle = 'rgb(255, 255, 255)';
+	ctx.globalAlpha = (this.VANISH_FRAME - this.frame_count + 1) * 0.05;
+	ctx.arc(r+2, r+2, r, 0, Math.PI * 2);
+	ctx.fill();
+
+	// 円の外枠
+	ctx.beginPath();
+	ctx.strokeStyle = 'rgb(255, 255, 255)';
+	ctx.globalAlpha = (this.VANISH_FRAME - this.frame_count + 1) * 0.1;
+	ctx.lineWidth = 3;
+	ctx.arc(r+2, r+2, r, 0, Math.PI * 2);
+	ctx.stroke();
+
+	this.game.surface.drawImage(cvs, x-r-2, y-r-2);
 };
 
 module.exports = Effect;
@@ -18152,11 +18189,8 @@ Enemy.prototype.notifyCollision = function(obj) {
 		// スコアの加算
 		this.stage.score += 100;
 
-		// TODO: 死亡エフェクト再生
-		/*
-		this.effectManager.createExplosion(enemy);
-		this.effectManager.create(enemy, 'shockwave', null) ;
-		*/
+		// 死亡エフェクト再生
+		this.stage.effectmanager.create(this);
 
 		// ポイントアイテムの生成
 		if(this.powerItem || this.scoreItem) {
